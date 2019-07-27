922ed8dea2d9394244ceaedf0b0ef5e1524a8af3
dnnycortes <daniela.munozc@gmail.com>
2019-07-27 15:01:59 -0500

    Feature | Migrating sqlite3 database

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*)qg_%em&h#(^ao(sx4tx+y61quf*@z+dzc__dcl#5=amr@awk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
