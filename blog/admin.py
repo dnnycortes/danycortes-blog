72aa3e0c404bbbf52fcaa0bebb958028b55c06cd
Daniela Cortes <danycortes@Danielas-MacBook-Air.local>
2019-07-30 11:51:46 -0500

    Revert "Fix | Removing files untracked"
    This reverts commit c9be282426c89f37b98b7b37c564ef96a01cd472.

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Post
+
+# Registering Post Model into Admin Page
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Model es el esqueleto / Oject es la instancia del modelo, ya contiene datos
+# models.Model means that is a Django Model, so that it should be saved in the database.
+# models.CharField – this is how you define text with a limited number of characters.
+# models.TextField – this is for long text without a limit. Sounds ideal for blog post content, right?
+# models.DateTimeField – this is a date and time.
+# models.ForeignKey – this is a link to another model.
+
+class Post( models.Model ):
+    author = models.ForeignKey( settings.AUTH_USER_MODEL, on_delete=models.CASCADE )
+    title = models.CharField( max_length=200 )
+    text = models.TextField()
+    created_date = models.DateTimeField( default=timezone.now )
+    published_date = models.DateTimeField( blank=True, null=True )
+
+    def publish( self ):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__( self ):
+        return self.title
